3b09ec4f8ec47aa2c32eb49228e604ce7aec571c
Sameer Kolpekwar <kolpekwarss15.comp@coep.ac.in>
2017-04-15 23:58:26 +0530

    Main Class for a torrent file

---
+++ Torrent.py
@@ -0,0 +1,31 @@
+#Class to handle all aspects of torrent
+import struct
+import socket
+from Tracker import Tracker
+from MessagesAndHandshakes import Handshake
+from Metainfo import Metainfo
+from FileReadWrite import FileReadWrite
+from RequestManager import Requester
+class  Torrent(object):
+    'An object to handle all Torrent Activity'
+    def __init__(self):
+        self.file_addr         = 'dd'
+        self.metainfo          = Metainfo(self.file_addr)
+        self.tracker           = Tracker(self, self.metainfo)
+        self.handshake         = Handshake(self.metainfo, self.tracker)
+        self.file_handler      = FileReadWrite(self.metainfo)
+        self.requester         = Requester(self.metainfo, self.file_handler)
+        self.peer_list         = list()
+        self.buildPeerList()
+
+    def buildPeerList(self):
+        'Builds List of Peers from Tracker Data'
+        temp_data = self.tracker.peers
+        offset = 0
+        peer = dict()
+        while offset != len(temp_data):
+            packed_peer, packed_port = struct.unpack_from("!iH", temp_data, offset)
+            offset += 6
+            peer['ip'] = socket.inet_ntoa(struct.pack("!i", packed_peer))
+            peer['port'] = packed_port
+            self.peer_list.append(peer)
